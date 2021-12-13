90733cb9e989d6476ebbbcae2c4541fd705ba1b6
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-12-13 16:31:12 -0500

    Update ALTester.java

--- lab02/ALTester.java
+++ lab02/ALTester.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class ALTester {
 
-public static boolean isSorted(ArrayList<Integer> a) {
-		for (int i = 0; i < a.size() - 1; i++) {
+public static boolean check(ArrayList<Integer> a) {
+	for (int i = 0; i < a.size() - 1; i++) {
 			if (a.get(i) > a.get(i + 1))
 				return false;
 		}
@@ -16,12 +16,8 @@ public static boolean isSorted(ArrayList<Integer> a) {
 	public static void main(String[] args) {
 		for (int i = 0; i < 23; i++)
 			
-			OrderedArrayList.add(_data, 23 - i);
-
-		
+		OrderedArrayList.add(_data, 23 - i);		
 		printArrayList(_data);
-
-		
 		System.out.println(isSorted(_data));
 		_data.add(50);
 	}
